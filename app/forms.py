0e03e8b8d8dae10acc01b2406edff8373462f638
cactus-code <cactuscodenz@gmail.com>
2018-05-16 21:48:43 +1200

    Commit

--- app/forms.py
+++ app/forms.py
@@ -33,8 +33,12 @@ class EditProfileForm(FlaskForm):
     username = StringField('New Username', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Name is taken. Please use a different username.')
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
 
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('This username is already taken.')
